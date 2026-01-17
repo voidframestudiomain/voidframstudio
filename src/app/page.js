d96295984d5e27513939450dca59f0753f52f8c1
Mridul-Works <mridul2431@gmail.com>
2026-01-17 20:03:00 +0530

    try

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,6 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <h1 className={styles.textCenter} > Comming Soon / in next </h1>
+    <h1 className={styles.textCenter} > Comming Soon / in next new change </h1>
   );
 }
