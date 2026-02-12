dd546e77a6c81e692b6454257681b251309eb7bb
Mridul-Works <mridul2431@gmail.com>
2026-02-13 01:18:34 +0530

    build fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export',      // required to generate /out folder
+  reactCompiler: true,   // keeps your existing config
 };
 
 export default nextConfig;
